d0d05d262648a51fdc77d5548a7284164e3835a4
Lukasz Pieczonka <pieczarka51@gmail.com>
2020-03-14 19:30:26 +0100

    Creating and throwing custom exception

--- APM-Begin/APM.SL/Discount.cs
+++ APM-Begin/APM.SL/Discount.cs
@@ -1,26 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using APM.Utilities;
 
 namespace APM.SL
 {
-  public class Discount
-  {
-    public int DiscountId { get; private set; }
+    public class Discount
+    {
+        public int DiscountId { get; private set; }
         //After enabling nullable feature - we can either set non-null default value or use nullable type - ? oprator
         public string DiscountName { get; set; } = string.Empty;
 
-    public decimal? PercentOff { get; set; }
+        public decimal? PercentOff { get; set; }
 
-    // ... Discount details
+        // ... Discount details
 
-    public Discount? FindDiscount(List<Discount>? discounts, string discountName)
-    {
-      if (discounts is null) return null;
+        public Discount? FindDiscount(List<Discount>? discounts, string discountName)
+        {
+            if (discounts is null) return null;
 
-      var foundDiscount = discounts.Find(d => d.DiscountName == discountName);
+            var foundDiscount = discounts.Find(d => d.DiscountName == discountName);
 
-      return foundDiscount;
+            return foundDiscount;
+        }
+
+        public Discount? FindDicountWithException(List<Discount>? discounts, string discountName)
+        {
+            if (discounts is null) throw new ArgumentException("No discounts found");
+
+            var foundDiscount = discounts.Find(d => d.DiscountName == discountName);
+
+            if (foundDiscount is null)
+                throw new DiscountNotFoundException("Discount not found");
+            return foundDiscount;
+        }
     }
-
-  }
 }
---
+++ APM-Begin/APM.Utilities/DiscountNotFoundException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APM.Utilities
+{
+    [Serializable()]
+    public class DiscountNotFoundException :System.Exception
+    {
+        public DiscountNotFoundException()
+        {
+
+        }
+
+        public DiscountNotFoundException(string message) : base(message)
+        {
+
+        }
+
+        // useful when we want to throw our exception as a result of a prior exception 
+        public DiscountNotFoundException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+        // to make exception serializable, 
+        protected DiscountNotFoundException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context): base(info, context)
+        {
+
+        }
+    }
+}
